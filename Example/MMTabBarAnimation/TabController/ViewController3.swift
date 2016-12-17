ed94301ba6a6ad6923f8bf1abd0143412503c45b
Millman YANG <millmanyang@gmail.com>
2016-12-17 22:45:57 +0800

    no message

---
+++ Example/MMTabBarAnimation/TabController/ViewController3.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController3.swift
+//  MMTabBarAnimation
+//
+//  Created by Millman YANG on 2016/12/17.
+//  Copyright © 2016年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
